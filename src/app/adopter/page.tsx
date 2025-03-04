38ea91d6d0f16892c0c02d71ea2823419e7a5309
Anne Choulet <anne.choulet@free.fr>
2025-03-04 18:01:22 +0100

    first commit

---
+++ src/app/adopter/page.tsx
@@ -0,0 +1,5 @@
+export default function Adopter(){
+    return (
+        <h1>Adopter</h1>
+    )
+}
---
+++ src/app/animals/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function AnimalList() {
+    return (
+        <>
+        <h1>Liste des animaux</h1>
+        <Link href="animals/1"></Link>
+        </>
+    )
+}
